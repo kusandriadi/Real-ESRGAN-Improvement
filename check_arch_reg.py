c9892562c5286ffd3c0c8f447393bcc615c652c3
Kus Andriadi <me@kusandriadi.com>
2025-04-30 22:27:10 +0700

    Add new training configurations and experiment setups
    Added multiple YAML configuration files for training RealESRGAN models with various experiment setups and dataset conditions. Updated `train.py` to streamline imports and added `.idea` to `.gitignore` for better environment management. Adjustments were made to improve validation and logging behavior, as well as experiment-specific hyperparameters.

---
+++ check_arch_reg.py
@@ -0,0 +1,2 @@
+from basicsr.utils.registry import ARCH_REGISTRY
+print("Registered architectures:", ARCH_REGISTRY.keys())
---
+++ realesrgan/archs/discriminator_patchgan_arch.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+from basicsr.utils.registry import ARCH_REGISTRY
+
+
+@ARCH_REGISTRY.register()
+class PatchGANDiscriminator(nn.Module):
+    def __init__(self, input_channels=3, num_filters=64):
+        super(PatchGANDiscriminator, self).__init__()
+        layers = [
+            nn.Conv2d(input_channels, num_filters, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True)
+        ]
+        nf = num_filters
+        for i in range(1, 3):  # Reduce the depth for a lightweight version
+            layers += [
+                nn.Conv2d(nf, nf * 2, kernel_size=4, stride=2, padding=1),
+                nn.BatchNorm2d(nf * 2),
+                nn.LeakyReLU(0.2, inplace=True)
+            ]
+            nf *= 2
+        layers += [nn.Conv2d(nf, 1, kernel_size=4, stride=1, padding=1)]
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
